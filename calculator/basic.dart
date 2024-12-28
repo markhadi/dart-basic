435596a5b73c8e42f68d2b2be6714a23aed41b53
Markhadi <markhadi@protonmail.com>
2024-12-28 22:08:49 +0800

    Create basic.dart

---
+++ calculator/basic.dart
@@ -0,0 +1,17 @@
+void main() {
+  // 1. Variables & Data Types
+  int number1 = 10;
+  int number2 = 5;
+
+  // 2. Basic Operations
+  int resultAdd = number1 + number2;
+  int resultSubtract = number1 - number2;
+  int resultMultiply = number1 * number2;
+  double resultDivide = number1 / number2;
+
+  // 3. Print Output
+  print('ResultAddition: $resultAdd');
+  print('ResultSubtraction: $resultSubtract');
+  print('ResultMultiplication: $resultMultiply');
+  print('ResultDivision: $resultDivide');
+}
